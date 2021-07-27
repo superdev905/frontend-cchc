32a894210bb75eb63c46623ddfbbf814bb0d8b5e
superdev905 <superdev905@gmail.com>
2021-07-27 17:08:02 -0400

    boton actualizar empresa, corregido

--- src/components/Companies/Create/StepTwo.js
+++ src/components/Companies/Create/StepTwo.js
@@ -233,7 +233,7 @@ const StepOne = () => {
           success={success}
           disabled={!formik.isValid || formik.isSubmitting}
         >
-          {create.type === 'UPDATE' ? 'Actualidar' : 'Crear'} empresa
+          {create.type === 'UPDATE' ? 'Actualizar' : 'Crear'} empresa
         </SubmitButton>
       </Box>
       <CreateModal open={open} onClose={toggleOpen} />
