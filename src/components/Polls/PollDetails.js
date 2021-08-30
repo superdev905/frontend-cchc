77c006c27d1fe5e21eb3b175490c425c0df49392
superdev905 <superdev905@gmail.com>
2021-08-30 18:09:11 -0400

    changes in poll details

--- src/components/Polls/PollDetails.js
+++ src/components/Polls/PollDetails.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles(() => ({
   }
 }))
 
-const PollDetails = () => {
+const PollDetails = ({ fetching }) => {
   const classes = useStyles()
   const dispatch = useDispatch()
   const { poll } = useSelector((state) => state.poll)
