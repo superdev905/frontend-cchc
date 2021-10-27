af2daf13071e52ec51aaa8ac2de96c1b354015f8
superdev905 <superdev905@gmail.com>
2021-10-27 17:32:14 -0300

    activity timeline changed

--- src/components/Benefit/ActivitiesTab.js
+++ src/components/Benefit/ActivitiesTab.js
@@ -1,42 +1,10 @@
-import { useParams } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
 import { Box } from '@material-ui/core'
-import { useToggle } from '../../hooks'
-import { Button } from '../UI'
-import CreateActivity from '../Benefits/CreateActivity'
-import benefitsActions from '../../state/actions/benefits'
 import ActivityTimeLine from './ActivityTimeline'
 
-const ActivitiesTab = () => {
-  const dispatch = useDispatch()
-  const { benefitId } = useParams()
-  const { open: openAdd, toggleOpen: toggleOpenAdd } = useToggle()
-
-  const createActivity = (values) =>
-    dispatch(
-      benefitsActions.createActivity({
-        ...values,
-        benefitId,
-        createdDate: new Date(),
-        isActive: true
-      })
-    )
-
-  return (
-    <Box>
-      <Box>
-        <Button onClick={toggleOpenAdd}>Agregar</Button>
-        <ActivityTimeLine />
-      </Box>
-      {openAdd && (
-        <CreateActivity
-          open={openAdd}
-          onClose={toggleOpenAdd}
-          submitFunction={createActivity}
-        />
-      )}
-    </Box>
-  )
-}
+const ActivitiesTab = () => (
+  <Box>
+    <ActivityTimeLine />
+  </Box>
+)
 
 export default ActivitiesTab
