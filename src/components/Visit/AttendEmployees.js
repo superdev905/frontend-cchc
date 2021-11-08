30d7565605a5940dd83e2e6e26d24952cd1eab0e
superdev905 <superdev905@gmail.com>
2021-11-08 17:46:22 -0500

    wip(): export employee to attend

--- src/components/Visit/AttendEmployees.js
+++ src/components/Visit/AttendEmployees.js
@@ -1,49 +1,78 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { FiDownload as ExportIcon } from 'react-icons/fi'
 import { Box, Typography } from '@material-ui/core'
 import { DataTable } from '../Shared'
-import { Wrapper } from '../UI'
+import { SubmitButton, Wrapper } from '../UI'
+import assistanceActions from '../../state/actions/assistance'
 
-const ContactList = () => (
-  <Wrapper>
-    <Box p={1}>
-      <Typography
-        style={{ fontSize: '18px', fontWeight: 'bold', marginBottom: '10px' }}
+const ContactList = () => {
+  const dispatch = useDispatch()
+  const [exporting, setExporting] = useState(false)
+  const { idVisit } = useParams()
+
+  const handleExportEmployees = () => {
+    setExporting(true)
+    dispatch(assistanceActions.exportEmployeesToAttend(idVisit)).then(() => {
+      setExporting(false)
+    })
+  }
+  return (
+    <Wrapper>
+      <Box
+        p={1}
+        display={'flex'}
+        justifyContent={'space-between'}
+        alignItems={'center'}
       >
-        Trabajadores por atender
-      </Typography>
-    </Box>
-    <DataTable
-      emptyMessage="No hay trabajadores por atender"
-      columns={[
-        {
-          name: 'Run',
-          selector: (row) => row.full_name,
-          sortable: true
-        },
-        {
-          name: 'Nombres',
-          selector: (row) => row.full_name,
-          sortable: true
-        },
-        {
-          name: 'Apellidos',
-          selector: (row) => row.charge
-        },
-        {
-          name: 'Nacionalidad',
-          selector: (row) => row.email
-        },
-        {
-          name: 'Sexo',
-          selector: (row) => row.email
-        },
-        {
-          name: 'Fecha de nacimiento',
-          selector: (row) => row.email
-        }
-      ]}
-      data={[]}
-    />
-  </Wrapper>
-)
+        <Typography
+          style={{ fontSize: '18px', fontWeight: 'bold', marginBottom: '10px' }}
+        >
+          Trabajadores por atender
+        </Typography>
+        <SubmitButton
+          loading={exporting}
+          onClick={handleExportEmployees}
+          startIcon={<ExportIcon />}
+        >
+          Exportar a excel
+        </SubmitButton>
+      </Box>
+      <DataTable
+        emptyMessage="No hay trabajadores por atender"
+        columns={[
+          {
+            name: 'Run',
+            selector: (row) => row.full_name,
+            sortable: true
+          },
+          {
+            name: 'Nombres',
+            selector: (row) => row.full_name,
+            sortable: true
+          },
+          {
+            name: 'Apellidos',
+            selector: (row) => row.charge
+          },
+          {
+            name: 'Nacionalidad',
+            selector: (row) => row.email
+          },
+          {
+            name: 'Sexo',
+            selector: (row) => row.email
+          },
+          {
+            name: 'Cargo de obra',
+            selector: (row) => row.email
+          }
+        ]}
+        data={[]}
+      />
+    </Wrapper>
+  )
+}
 
 export default ContactList
