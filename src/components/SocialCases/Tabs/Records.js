ea33f62e3784ccd783dacd1d2b99a816d1a9805a
superdev905 <superdev905@gmail.com>
2021-12-10 11:53:14 -0500

    adding personal intervention records

--- src/components/SocialCases/Tabs/Records.js
+++ src/components/SocialCases/Tabs/Records.js
@@ -1,5 +1,43 @@
-import { Box } from '@material-ui/core'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box, Typography } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+import assistanceActions from '../../../state/actions/assistance'
+import { AssistanceList } from '../../Visit'
+import useStyles from './styles'
 
-const Background = () => <Box>Registros</Box>
+const Record = () => {
+  const classes = useStyles()
+  const dispatch = useDispatch()
+  const { socialCaseId } = useParams()
+  const [list, setList] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [query] = useState({ page: 1, size: 30, socialCaseId })
 
-export default Background
+  const fetchRecords = () => {
+    setLoading(false)
+    dispatch(assistanceActions.getAttention(query))
+      .then((res) => {
+        setLoading(false)
+        setList(res)
+      })
+      .catch(() => {
+        setLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    fetchRecords()
+  }, [query])
+
+  return (
+    <Box>
+      <Typography className={classes.title}>
+        Registros de Intervención personal
+      </Typography>
+      <AssistanceList list={list} loading={loading} />
+    </Box>
+  )
+}
+
+export default Record
