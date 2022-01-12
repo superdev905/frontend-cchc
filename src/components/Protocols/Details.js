6febf0b0d93ea8a5f0699076d2fc6cb7e069e66b
superdev905 <superdev905@gmail.com>
2022-01-12 13:10:06 -0300

    avance en detalles de protocolo

---
+++ src/components/Protocols/Details.js
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import { FiArrowLeft as ArrowBack } from 'react-icons/fi'
+import { useSelector } from 'react-redux'
+import { Box, Chip, Drawer, IconButton } from '@material-ui/core'
+import { LabeledRow, Text } from '../UI'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '80%'
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: 600
+    }
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  heading: {
+    fontSize: '17px',
+    fontWeight: 'bold',
+    marginTop: '10px'
+  }
+}))
+
+const DetailsDraw = ({ open, onclose, protocolId }) => {
+  const { isMobile } = useSelector((state) => state.ui)
+  const [loading, setLoading] = useState(false)
+
+  return (
+    <Drawer
+      classes={{ paper: classes.root }}
+      anchor="right"
+      fullScreen={isMobile}
+      open={open}
+      onClose={onclose}
+    >
+      <Box px={3} py={4}>
+        <Box display="flex" alignItems="center">
+          <IconButton onClick={onClose}>
+            <ArrowBack />
+          </IconButton>
+          <Text loading={loading} className={classes.title}>
+            {`${protocol?.title}`}
+          </Text>
+        </Box>
+        <Box my={2}>
+          <LabeledRow loaderWidth="5%" label={'Clasificación'}>
+            <Text loading={loading}>{protocol?.classification}</Text>
+          </LabeledRow>
+          <LabeledRow loaderWidth="5%" label={'Fecha de Inicio'}>
+            <Text loading={loading}>{formatDate(protocol?.startDate)}</Text>
+          </LabeledRow>
+          <LabeledRow loaderWidth="5%" label={'Fecha de Fin'}>
+            <Text loading={loading}>{formatDate(protocol?.endDate)}</Text>
+          </LabeledRow>
+          <Chip color="primary" label={item.module.name} />
+        </Box>
+      </Box>
+    </Drawer>
+  )
+}
+
+export default DetailsDraw
