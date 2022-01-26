bf0d2171d6e4834f868847edb38cbb08cdb20c79
superdev905 <superdev905@gmail.com>
2022-01-26 12:03:54 -0500

    fix(): company tree data at facturation modal

--- src/components/Constructions/CreateModal.js
+++ src/components/Constructions/CreateModal.js
@@ -219,7 +219,18 @@ const ConstructionModal = ({
       dispatch(companiesActions.getCompanies({ state: 'CREATED' }, false)).then(
         (list) => {
           setCompanies(list)
-          setTreeData(buildTreeData(list))
+          if (type !== 'UPDATE') {
+            setTreeData(buildTreeData(list))
+          } else {
+            const treeList = buildTreeData(list).map((item) =>
+              searchFromTree(
+                item,
+                item,
+                parseInt(construction?.billing_business_id, 10)
+              )
+            )
+            setTreeData(treeList.filter((item) => item))
+          }
         }
       )
     }
