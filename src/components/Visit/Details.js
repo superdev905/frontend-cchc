4d79e187d17d61f8a50d8ae41a84626e3313fa71
superdev905 <superdev905@gmail.com>
2022-01-26 15:27:57 -0300

    button 'cancelar' change to disable when send the report or is closed in visit

--- src/components/Visit/Details.js
+++ src/components/Visit/Details.js
@@ -225,7 +225,9 @@ const Details = ({ fetching, fetchDetails }) => {
           disabled={
             visit?.status === 'CANCELADA' ||
             visit?.status === 'TERMINADA' ||
-            visit?.is_close
+            visit?.is_close_pending ||
+            visit?.is_close ||
+            visit?.is_close_pending
           }
         >
           Cancelar
