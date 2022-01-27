569db5ab4bda6f98da31ae1ea7185de9ab7377b9
superdev905 <superdev905@gmail.com>
2022-01-27 16:37:52 -0500

    fix(): simple card height

--- src/components/UI/SimpleCard.js
+++ src/components/UI/SimpleCard.js
@@ -5,22 +5,23 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.common.white,
     textAlign: 'center',
     borderRadius: theme.spacing(1),
-    minHeight: height || 130,
-    maxHeight: height || 130
+    minHeight: height || 80,
+    maxHeight: height || 80
   }),
   data: {
-    fontSize: 38,
+    fontSize: 28,
     fontWeight: 'bold'
   },
   label: {
-    opacity: 0.8
+    opacity: 0.8,
+    fontSize: 15
   }
 }))
 
 const SimpleCard = ({ data, label, height }) => {
   const classes = useStyles({ height })
   return (
-    <Box p={2} className={classes.cardRoot}>
+    <Box p={1} className={classes.cardRoot}>
       <Box display="flex" justifyContent="center" padding={0} margin={0}>
         <Typography className={classes.data}>{data}</Typography>
       </Box>
