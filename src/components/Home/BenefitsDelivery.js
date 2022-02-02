f3010579fbb1f80aa08c7ca6eda12f3a1e4e4762
superdev905 <superdev905@gmail.com>
2022-02-02 13:43:23 -0300

    Maqueta Home

---
+++ src/components/Home/BenefitsDelivery.js
@@ -0,0 +1,41 @@
+import { Box, Grid, makeStyles, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  cardRoot: ({ height }) => ({
+    backgroundColor: theme.palette.common.white,
+    borderRadius: theme.spacing(2),
+    minHeight: height || 80,
+    maxHeight: height || 130,
+    padding: theme.spacing(4)
+  }),
+  name: {
+    textAlign: 'center'
+  }
+}))
+
+const BenefitsDelivery = ({ height }) => {
+  const classes = useStyles({ height })
+
+  return (
+    <Box className={classes.cardRoot}>
+      <Grid container spacing={4}>
+        <Grid item xs={12} className={classes.name}>
+          <Typography variant="subtitle1">Nombre de Beneficio</Typography>
+        </Grid>
+        <Grid container justifyContent="space-evenly">
+          <Grid item>
+            <Typography variant="body2">Estado</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body2">Fecha de Inicio</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body2">Fecha de Fin</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+export default BenefitsDelivery
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,40 @@
-const Home = () => <div></div>
+import { Box, Typography, makeStyles } from '@material-ui/core'
+import VisitsPending from '../../components/Home/VisitsPending'
+import BenefitsDelivery from '../../components/Home/BenefitsDelivery'
+import Atentions from '../../components/Home/Atentions'
+
+const useStyles = makeStyles((theme) => ({
+  tittle: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 600,
+    color: '#6A707E',
+    textTransform: 'uppercase',
+    [theme.breakpoints.up('lg')]: {
+      fontSize: 22
+    }
+  }
+}))
+
+const Home = () => {
+  const classes = useStyles()
+
+  return (
+    <Box display="flex" justifyContent="space-evenly" mt={2}>
+      <Box>
+        <Typography className={classes.tittle}>Visitas Pendientes</Typography>
+        <VisitsPending />
+      </Box>
+      <Box>
+        <Typography className={classes.tittle}>
+          Beneficios Entregados
+        </Typography>
+        <BenefitsDelivery />
+        <Typography className={classes.tittle}>Atenciones</Typography>
+        <Atentions />
+      </Box>
+    </Box>
+  )
+}
 
 export default Home
