e21332e0a9b8bc91e0d51b7595246c3856c499cc
superdev905 <superdev905@gmail.com>
2022-04-27 23:39:49 -0400

    fix

--- src/components/SocialCases/Tabs/Background.js
+++ src/components/SocialCases/Tabs/Background.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Box, Grid, Typography, makeStyles } from '@material-ui/core'
-import { LabeledRow, Text, Wrapper } from '../../UI'
+import { LabeledRow, Text } from '../../UI'
 
 const useStyles = makeStyles(() => ({
   heading: {
@@ -17,106 +17,112 @@ const Background = ({ loading }) => {
   const { employee } = useSelector((state) => state.employees)
 
   return (
-    <Wrapper>
-      <Box>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Typography style={{ fontSize: '19px', fontWeight: 'bold' }}>
-            Detalles de Caso Social
-          </Typography>
-        </Box>
-        <Box p={2}>
-          <Grid container>
-            <Grid item xs={12} md={6}>
-              <Typography className={classes.heading}>
-                Datos del Trabajador
-              </Typography>
-              <LabeledRow label={'Run'}>
-                <Text loading={loading}>{caseDetails?.employee?.run} </Text>
-              </LabeledRow>
-              <LabeledRow label={'Nombres'}>
-                <Text loading={loading}>{caseDetails?.employee?.names}</Text>
-              </LabeledRow>
-              <LabeledRow label={'Apellido paterno'}>
-                <Text loading={loading}>
-                  {caseDetails?.employee?.paternalSurname}
-                </Text>
-              </LabeledRow>
-              <LabeledRow label={'Apellido materno'}>
-                <Text loading={loading}>
-                  {caseDetails?.employee?.maternalSurname}
-                </Text>
-              </LabeledRow>
-              <LabeledRow label={'Edad'}>
-                <Text loading={loading}>{caseDetails?.employee?.run} </Text>
-              </LabeledRow>
-              <LabeledRow label={'Estado Civil'}>
-                <Text loading={loading}>
-                  {employee?.marital_status?.description}{' '}
-                </Text>
-              </LabeledRow>
-              <LabeledRow label={'Hijos'}>
-                <Text loading={loading}>{caseDetails?.employee?.run} </Text>
-              </LabeledRow>
-              <LabeledRow label={'Afp'}>
-                <Text loading={loading}>{caseDetails?.employee?.run} </Text>
-              </LabeledRow>
-              <LabeledRow label={'Prevision'}>
-                <Text loading={loading}>{caseDetails?.employee?.run} </Text>
-              </LabeledRow>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography className={classes.heading}>
-                Detalle de Empresa
-              </Typography>
-              <LabeledRow label={'Run'}>
-                <Text loading={loading}>{caseDetails?.business?.rut}</Text>
-              </LabeledRow>
-              <LabeledRow label={'Nombre'}>
-                <Text loading={loading}>
-                  {caseDetails?.business?.businessName}
-                </Text>
-              </LabeledRow>
-              <LabeledRow label={'Email'}>
-                <Text loading={loading}>{caseDetails?.business?.email}</Text>
-              </LabeledRow>
-              <LabeledRow label={'Dirección'}>
-                <Text loading={loading}>{caseDetails?.business?.address}</Text>
-              </LabeledRow>
-            </Grid>
-          </Grid>
-
-          <Grid container>
-            <Grid item xs={12} md={6}>
-              <Typography className={classes.heading}>Area</Typography>
-              <LabeledRow label={'Nombre'}>
-                <Text loading={loading}>{caseDetails?.area?.name}</Text>
-              </LabeledRow>
-              <LabeledRow label={'Tema'}>
-                <Text loading={loading}>{caseDetails?.area.tema}</Text>
-              </LabeledRow>
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <Typography className={classes.heading}>
-                Información adicional
-              </Typography>
-
-              <LabeledRow width={200} label={'Profesional'}>
-                <Text
-                  loading={loading}
-                >{`${caseDetails?.professional?.names} ${caseDetails?.professional?.paternalSurname} ${caseDetails?.professional?.maternalSurname}`}</Text>
-              </LabeledRow>
-              <LabeledRow width={200} label={'Tipo de Solicitud'}>
-                <Text loading={loading}>{caseDetails?.requestType}</Text>
-              </LabeledRow>
-              <LabeledRow label={'Comentario'}>
-                <Text loading={loading}>{caseDetails?.comments}</Text>
-              </LabeledRow>
-            </Grid>
-          </Grid>
-        </Box>
+    <Box>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Typography
+          style={{ fontSize: '19px', fontWeight: 'bold', marginLeft: '1%' }}
+        >
+          Detalles de Caso Social
+        </Typography>
       </Box>
-    </Wrapper>
+      <Box p={2}>
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <Typography className={classes.heading}>
+              Datos del Trabajador
+            </Typography>
+            <LabeledRow label={'Run'}>
+              <Text loading={loading}>{caseDetails?.employee?.run} </Text>
+            </LabeledRow>
+            <LabeledRow label={'Nombres'}>
+              <Text loading={loading}>{caseDetails?.employee?.names}</Text>
+            </LabeledRow>
+            <LabeledRow label={'Apellido paterno'}>
+              <Text loading={loading}>
+                {caseDetails?.employee?.paternalSurname}
+              </Text>
+            </LabeledRow>
+            <LabeledRow label={'Apellido materno'}>
+              <Text loading={loading}>
+                {caseDetails?.employee?.maternalSurname}
+              </Text>
+            </LabeledRow>
+            <LabeledRow label={'Edad'}>
+              <Text loading={loading}>{caseDetails?.employee?.run} </Text>
+            </LabeledRow>
+            <LabeledRow label={'Región'}>
+              <Text loading={loading}></Text>
+            </LabeledRow>
+            <LabeledRow label={'Comuna'}>
+              <Text loading={loading}></Text>
+            </LabeledRow>
+            <LabeledRow label={'Estado Civil'}>
+              <Text loading={loading}>
+                {employee?.marital_status?.description}{' '}
+              </Text>
+            </LabeledRow>
+            <LabeledRow label={'Hijos'}>
+              <Text loading={loading}> </Text>
+            </LabeledRow>
+            <LabeledRow label={'Afp'}>
+              <Text loading={loading}></Text>
+            </LabeledRow>
+            <LabeledRow label={'Prevision'}>
+              <Text loading={loading}></Text>
+            </LabeledRow>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography className={classes.heading}>
+              Detalle de Empresa
+            </Typography>
+            <LabeledRow label={'Run'}>
+              <Text loading={loading}>{caseDetails?.business?.rut}</Text>
+            </LabeledRow>
+            <LabeledRow label={'Nombre'}>
+              <Text loading={loading}>
+                {caseDetails?.business?.businessName}
+              </Text>
+            </LabeledRow>
+            <LabeledRow label={'Email'}>
+              <Text loading={loading}>{caseDetails?.business?.email}</Text>
+            </LabeledRow>
+            <LabeledRow label={'Dirección'}>
+              <Text loading={loading}>{caseDetails?.business?.address}</Text>
+            </LabeledRow>
+          </Grid>
+        </Grid>
+
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <Typography className={classes.heading}>Area</Typography>
+            <LabeledRow label={'Nombre'}>
+              <Text loading={loading}>{caseDetails?.area?.name}</Text>
+            </LabeledRow>
+            <LabeledRow label={'Tema'}>
+              <Text loading={loading}>{caseDetails?.area.tema}</Text>
+            </LabeledRow>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Typography className={classes.heading}>
+              Información adicional
+            </Typography>
+
+            <LabeledRow width={200} label={'Profesional'}>
+              <Text
+                loading={loading}
+              >{`${caseDetails?.professional?.names} ${caseDetails?.professional?.paternalSurname} ${caseDetails?.professional?.maternalSurname}`}</Text>
+            </LabeledRow>
+            <LabeledRow width={200} label={'Tipo de Solicitud'}>
+              <Text loading={loading}>{caseDetails?.requestType}</Text>
+            </LabeledRow>
+            <LabeledRow label={'Comentario'}>
+              <Text loading={loading}>{caseDetails?.comments}</Text>
+            </LabeledRow>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   )
 }
 
